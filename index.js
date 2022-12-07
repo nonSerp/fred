365af2ea0f6b52486e77b542ab33724a0934cc4e
nonSerp <fredfarwenel2010@gmail.com>
2022-12-07 10:16:27 +0000

    First version

---
+++ index.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+
+console.log(`
+Fred Farwenel 
+
+Nature Aficionado! Autodidact Developer whose formative years were in the slum of Duala/Gbalasua.
+
+Twitter: https://twitter.com/fredfarwenel
+Github: https://github.com/nonSerp
+Blog: https://libklout.com/
+
+`);
