463d098356ef4449a9ac35a6bb2e90876abba12a
nonSerp <fredfarwenel2010@gmail.com>
2022-12-09 12:54:03 +0000

    Major Version

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 #!/usr/bin/env node
 
-const clearConsole = require('clear-any-console');
+const pkgJSON = require('./package.json');
+const welcome = require('cli-welcome');
 
-// Clears the console.
-clearConsole();
+welcome({
+    title: pkgJSON.name,
+    tagLine: `Hadu?, nice to meet you`,
+    description: pkgJSON.description,
+    version: pkgJSON.version,
+    bgColor: `#FADC00`,
+    color: `#000000,`,
+    bold: true,
+    clear: true,
+});
 
 console.log(`
 Fred Farwenel 
