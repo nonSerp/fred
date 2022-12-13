518d8221110714ecb7909db0f6f64f3f8c3abc86
nonSerp <fredfarwenel2010@gmail.com>
2022-12-13 15:35:14 +0000

    Colors Ux

--- index.js
+++ index.js
@@ -2,25 +2,31 @@
 
 const pkgJSON = require('./package.json');
 const welcome = require('cli-welcome');
+const chalk = require('chalk');
+const log = console.log;
+const dim = chalk.dim;
+const italic = chalk.italic;
+const twitterClr = chalk.hex(`#1da1f2`).bold.inverse;
+const githubClr = chalk.hex(`#6cc644`).bold.inverse;
+const purple = chalk.hex(`#6937FF`).bold.inverse;
 
 welcome({
-    title: pkgJSON.name,
+    title: `Fred Farwenel`,
     tagLine: `Hadu?, nice to meet you`,
     description: pkgJSON.description,
     version: pkgJSON.version,
-    bgColor: `#FADC00`,
+    bgColor: `#6937FF`,
     color: `#000000,`,
     bold: true,
     clear: true,
 });
 
-console.log(`
-Fred Farwenel 
-
-Nature Aficionado! Autodidact Developer whose formative years were in the slum of Duala/Gbalasua.
-
-Twitter: https://twitter.com/fredfarwenel
-Github: https://github.com/nonSerp
-Blog: https://libklout.com/
+log(`
+${italic(
+  `Nature Aficionado! Autodidact Developer whose formative years were in the slum of Duala/Gbalasua.`
+)}
+${twitterClr(` Twitter `)} ${dim(`https://twitter.com/fredfarwenel`)}
+${githubClr(` Github `)} ${dim(`https://github.com/nonSerp`)}
+${purple(`Blog `)} ${dim(`https://libklout.com/`)}
 
 `);
